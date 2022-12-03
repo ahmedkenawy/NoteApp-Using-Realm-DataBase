7f2a0ffdc1492a7cb6116eb1975d76ab55210aa3
Ahmed Kenawy <a7medsa3dkenawy@gmail.com>
2022-12-03 23:30:31 +0200

    adding helpful comments to code

--- app/src/main/java/com/a7medkenawy/noteapp/model/Note.kt
+++ app/src/main/java/com/a7medkenawy/noteapp/model/Note.kt
@@ -1,11 +1,12 @@
 package com.a7medkenawy.noteapp.model
 
-import android.os.Parcel
 import android.os.Parcelable
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
+//Note Table consists of 2 parameters 1-id (primary key) 2- content
+//annotated with (Parcelize) and implement (Parcelable) to send it easily between activities
 @Parcelize
 open class Note(
     @PrimaryKey
